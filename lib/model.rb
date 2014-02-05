ce01e0d4765768124488218bb911a9db7374d3f4
Rafer Hazen <rafer@ralua.com>
2014-02-05 12:34:44 -0700

    Move into top-level module, cleanup tests.

--- lib/model.rb
+++ lib/model.rb
@@ -1,5 +1,140 @@
 module Model
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def initialize(params={})
+    self.class.fields.select(&:collection?).each do |field|
+      params[field.name] ||= []
+    end
+    
+    params.each { |k,v | send("#{k}=", v) }
+  end
+
+  def errors
+    errors = Hash.new { [] }
+
+    self.class.fields.each do |field|
+      errors[field.name] = field.errors(self.send(field.name))
+    end
+
+    errors
+  end
+
+  module ClassMethods
+    def field(*args)
+      @fields ||= []
+      field = Field.new(*args)
+      attr_accessor(field.name)
+      fields << field
+    end
+    
+    def fields
+      @fields
+    end
+  end
+
+  class Field
+    attr_reader :name, :types
+
+    def initialize(name, options = {})
+      @name       = name
+      @types      = Array(options.fetch(:type, BasicObject))
+      @required   = options.fetch(:required, false)
+      @collection = options.fetch(:collection, false)
+      
+      if required? and collection?
+        raise ArgumentError, "fields cannot be both required a collection and required" 
+      end
+    end
+
+    def errors(value)
+      Validator.errors(self, value)
+    end
+
+    def required?
+      !!@required
+    end
+
+    def collection?
+      !!@collection
+    end      
+  end
+
+  class Validator
+    def self.errors(field, value)
+      validator = field.collection? ? Collection : Scalar
+      validator.new(field, value).errors
+    end
+
+    private
+    
+    attr_reader :field
+    
+    def types
+      field.types
+    end
+    
+    class Collection < Validator
+      def initialize(field, values)
+        @field, @values = field, values
+      end
+
+      def errors
+        case
+        when (not enumerable?)
+          ["was declared as a collection and is not enumerable"]
+        when type_mismatch?
+          ["contains a value that is not an instance of #{types.map(&:inspect).join(' or ')}"]
+        else
+          []
+        end
+      end
+
+      private
+
+      attr_reader :values
+
+      def enumerable?
+        values.is_a?(Enumerable)
+      end
+    
+      def type_mismatch?
+        values.all? do |value|
+          field.types.none? { |t| value.is_a?(t) }
+        end
+      end
+    end
+  
+    class Scalar < Validator
+      def initialize(field, value)
+        @field, @value = field, value
+      end
+
+      def errors
+        case
+        when (value.nil? and field.required?)
+          ["cannot be empty"]
+        when type_mismatch?
+          ["is not an instance of #{type_string} (was #{value.class.inspect})"]
+        else
+          []
+        end
+      end
+    
+      private 
+    
+      attr_reader :value
+                
+      def type_mismatch?
+        types.none? { |t| value.is_a?(t) } 
+      end
+      
+      def type_string
+        types.map(&:inspect).join(' or ')
+      end
+    end
+  end
 end
 
 require "model/version"
-require "model/model"
---
+++ spec/model_spec.rb
@@ -0,0 +1,136 @@
+require "spec_helper"
+
+describe Model do
+  describe ".field" do
+    it "creates a getter and setter for the field" do
+      define { field :name }
+
+      client = Client.new
+      client.name = "Rafer"
+
+      expect(client.name).to eq("Rafer")
+    end
+    
+    it "defaults :collection fields to an empty array" do
+      define { field :name, :collection => true }
+
+      client = Client.new
+      expect(client.name).to eq([])
+    end
+    
+    it "does not allow the :required attribute with :collection" do
+      required_collection = lambda do
+         define { field :name, :collection => true, :required => true }
+       end
+       
+      expect(&required_collection).to raise_error(ArgumentError, /fields cannot be both required a collection and required/ )
+    end
+  end
+
+  describe ".new" do
+    before { define { field :name } }
+
+    it "accepts fields" do
+      client = Client.new(:name => "Rafer")
+      expect(client.name).to eq("Rafer")
+    end
+  
+    it "raises an exception for unrecognized parmeters" do
+      expect { Client.new(:wrong => "") }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe ".errors" do
+    it "includes errors for incorrect types" do
+      define { field :field, :type => String }
+      client = Client.new(:field => :wrong)
+      
+      expect(client.errors[:field]).to include("is not an instance of String (was Symbol)")
+    end
+
+    it "include no errors for correct types" do
+      define { field :field, :type => String }
+      client = Client.new(:field => "right")
+
+      expect(client.errors[:field]).to be_empty
+    end
+
+    it "include no errors for correct subtypes" do
+      define { field :field, :type => Object }
+      client = Client.new(:field => "right")
+
+      expect(client.errors[:field]).to be_empty
+    end
+
+    it "includes errors for incorrect types (when multiple are specified)" do
+      define { field :field, :type => [String, Symbol] }
+      client = Client.new(:field => 1)
+      
+      expect(client.errors[:field]).to include("is not an instance of String or Symbol (was Fixnum)")
+    end
+    
+    it "includes no errors for correct types (when multiple types are specified)" do
+      define { field :field, :type => [String, Symbol] }
+
+      expect(Client.new(:field => "right").errors[:field]).to be_empty
+      expect(Client.new(:field => :right).errors[:field]).to be_empty
+    end
+
+    it "includes an error for required fields that are nil" do
+      define { field :field, :required => true }
+      client = Client.new(:field => nil)
+
+      expect(client.errors[:field]).to include("cannot be empty")
+    end
+    
+    it "includes only the 'empty' error for fields that are required and typed, with a nil value" do
+      define { field :field, :type => String, :required => true }
+      client = Client.new(:field => nil)
+
+      expect(client.errors[:field]).to include("cannot be empty")
+    end
+    
+    it "includes errors for incorrect types in collections" do
+      define { field :field, :type => String, :collection => true }
+      client = Client.new(:field => [:wrong])
+
+      expect(client.errors[:field]).to include("contains a value that is not an instance of String")
+    end
+
+    it "includes errors for incorrect types in collections (when multiple are types are specified)" do
+      define { field :field, :type => [String, Symbol], :collection => true }
+      client = Client.new(:field => [1])
+
+      expect(client.errors[:field]).to include("contains a value that is not an instance of String or Symbol")
+    end
+    
+    it "includes no errors errors when a collection has all the right types" do
+      define { field :field, :type => String, :collection => true }
+      client = Client.new(:field => ["right", "right"])
+
+      expect(client.errors[:field]).to be_empty
+    end
+    
+    it "includes an error when a collection is not enumerable" do
+      define { field :field, :type => String, :collection => true }
+      client = Client.new(:field => 1)
+      
+      expect(client.errors[:field]).to include("was declared as a collection and is not enumerable")
+    end
+
+    it "includes no errors for correct types (when multiple are specified, in a collection)" do
+      define { field :field, :type => [String, Symbol], :collection => true }
+      client = Client.new(:field => ["right", :right])
+    
+      expect(client.errors[:field]).to be_empty
+    end
+  end
+
+  def define(&block)
+    client = Class.new do
+      include Model
+      instance_eval(&block)
+    end
+    stub_const("Client", client)
+  end
+end
